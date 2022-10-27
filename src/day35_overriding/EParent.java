93a75114feb3d2a2c79acccab954faaffed26aa5
Emine Yucel <emn.yl.06@gmail.com>
2022-10-27 15:15:42 +0200

    first commit

---
+++ src/day35_overriding/EParent.java
@@ -0,0 +1,10 @@
+package day35_overriding;
+
+public class EParent {
+    void method1(){
+        System.out.println("parent method1");
+    }
+    void method2(){
+        System.out.println("parent method2");
+    }
+}
